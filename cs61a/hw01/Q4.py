96b51e6c6492a223fccf0b51107ee6d1e100adda
Richard Piccioni <rpiccioni@bayschoolsf.org>
2016-03-21 14:46:44 -0700

    Revert "submitting my answers to Q2,3,4 of Hw01."

--- cs61a/hw01/Q4.py
+++ cs61a/hw01/Q4.py
@@ -3,14 +3,8 @@ def largest_factor(n):
 
     >>> largest_factor(4) # n*n-1 is 15; factors are 1, 3, 5, 15
     3
-    >>> largest_factor(9) # n*n-1 is 80; factors are 1, 2, 4, 5, 8, 10, ...
+    >>> largest_factor(9) # n*n-1 is 80; factors are 1, 2, 4, 5, 8, 10, 
+...
     8
     """
-    maxFac = 0
-    for i in range(1, n):
-         if (n*n-1) % i == 0:
-             maxFac = i
-    return(maxFac)
-    
-print(largest_factor(4))
-
+    "*** YOUR CODE HERE ***"
